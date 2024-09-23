fc8ebacd5d58433a5663d881c79c3072641ddbe7
dharani535011 <dharani535011@gmail.com>
2024-09-23 17:23:26 +0530

    first commit

---
+++ src/Components/Otherprovider.jsx
@@ -0,0 +1,44 @@
+import axios from 'axios'
+import React, { createContext, useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+export const Othercontexts=createContext()
+
+const Otherprovider = ({children}) => {
+ const [loader,setloader]= useState(false)
+ const [user,setuser]= useState({})
+ const [userdet,setuserdet]= useState({})
+ const [auth,setauth]= useState(false)
+ const [fee,setfee]= useState([])
+ const [open, setopen] = useState(false)
+ const navigate=useNavigate()
+ 
+ const fetchdata = async () => {
+  try {
+    setloader(true)
+    const res = await axios.post("http://localhost:3000/users/checkauthen", {}, { withCredentials: true });
+
+    if (res.data.message === "on") {
+      setuser(res.data.data)
+      setauth(true)
+      navigate("/")
+    }
+  } catch (error) {
+    console.log("Error fetching data:", error.message);
+  } finally {
+    setloader(false)
+  }
+};
+useEffect(()=>{
+       fetchdata()
+},[auth])
+// console.log(user.fees)
+
+  return (
+    <Othercontexts.Provider value={{userdets:[userdet,setuserdet],opens:[open,setopen],fees:[fee,setfee],auths:[auth,setauth],loaders:[loader,setloader],users:[user,setuser]}}>
+       {children}
+    </Othercontexts.Provider>
+  )
+}
+
+export default Otherprovider
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Homebg from '../Components/Homebg'
+import Maxlife from '../Components/Maxlife'
+import Program from '../Components/Program'
+import Handn from '../Components/Handn'
+import Form from '../Components/Form'
+import Foot from '../Components/Foot'
+import Call from '../Components/Call'
+
+const Home = () => {
+  return (
+<>
+<Homebg/>
+<Maxlife/>
+<Program/>
+<Handn/>
+<Form/>
+<Foot/>
+<Call/>
+</>
+  )
+}
+
+export default Home
